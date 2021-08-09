6bf64e588ef1738ac64980e53006599fe0cce059
mtfrat <martinfraticelli@gmail.com>
2021-08-09 11:37:50 -0300

    Desafio clase 1

---
+++ main.js
@@ -0,0 +1,19 @@
+let numero1 = parseInt(prompt("Ingresa primer numero: "));
+
+let numero2 = parseInt(prompt("Ingresa primer numero: "));
+
+let suma = numero1 + numero2;
+
+let resta = numero1 - numero2;
+
+let multiplicacion = numero1 * numero2;
+
+let division = numero1 / numero2;
+
+console.log("El resultado de la suma es: ", suma);
+
+console.log("El resultado de la resta es: ", resta);
+
+console.log("El resultado de la multiplicacion es: ", multiplicacion);
+
+console.log("El resultado de la division es: ", division);
